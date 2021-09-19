0db39012ddea8ca1fe11fd43e002c379f88cf6df
Olawale Malomo <warlay234@gmail.com>
2021-09-19 17:49:12 +0100

    Add ratings functionality

--- src/components/Ratings/index.js
+++ src/components/Ratings/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import classnames from 'classnames';
 import onIcon from './icons/on.svg';
 import offIcon from './icons/off.svg';
@@ -6,23 +6,69 @@ import styles from './index.module.css';
 
 const ratings = [1, 2, 3, 4, 5];
 
-const RatingButtons = ({ratings, bgIcon}) =>
+const RatingButtons = ({
+  ratings,
+  bgIcon,
+  isActive,
+  onMouseOver,
+  onMouseOut,
+  onClick,
+}) =>
   ratings.map((rating) => (
-    <button key={rating} type="button" className={styles.btn} title={rating}>
+    <button
+      key={rating}
+      type="button"
+      className={styles.btn}
+      title={rating}
+      onClick={isActive ? () => onClick(rating) : null}
+      onMouseOver={isActive ? () => onMouseOver(rating) : null}
+      onMouseOut={isActive ? () => onMouseOut() : null}>
       <img src={bgIcon} alt={rating} />
     </button>
   ));
 
-const Ratings = ({value}) => {
-  const width = (value / 5) * 100;
+const Ratings = ({value, onChange, isActive}) => {
+  const [hoverValue, setHoverValue] = useState(0);
+  const width = ((hoverValue || value) / 5) * 100;
+
+  const handleMouseOver = useCallback((value) => {
+    setHoverValue(value);
+  }, []);
+
+  const handleClick = useCallback(
+    (value) => {
+      onChange(value);
+    },
+    [onChange],
+  );
+
+  const handleMouseOut = useCallback(() => {
+    setHoverValue(0);
+  }, []);
 
   return (
     <div className={styles.ratings}>
       <div className={classnames(styles.row, styles.off)}>
-        <RatingButtons ratings={ratings} bgIcon={offIcon} />
+        <RatingButtons
+          ratings={ratings}
+          bgIcon={offIcon}
+          isActive={isActive}
+          onClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        />
       </div>
-      <div className={classnames(styles.row, styles.on)} style={{width}}>
-        <RatingButtons ratings={ratings} bgIcon={onIcon} />
+      <div
+        className={classnames(styles.row, styles.on)}
+        style={{width: `${width}%`}}>
+        <RatingButtons
+          ratings={ratings}
+          bgIcon={onIcon}
+          isActive={isActive}
+          onClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        />
       </div>
     </div>
   );
--- src/components/ReviewModal/index.js
+++ src/components/ReviewModal/index.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import PrimaryButton from 'components/PrimaryButton';
 import closeIcon from 'icons/close.svg';
 import styles from './index.module.css';
 import Ratings from 'components/Ratings';
 
-const ReviewModal = ({ onClose }) => {
+const ReviewModal = ({onClose}) => {
+  const [ratings, setRatings] = useState(0);
+
+  const handleRatingsChange = useCallback((value) => {
+    setRatings(value);
+  }, []);
+
   return (
-    <div className={ styles.modal }>
-      <div className={ styles.overlay }/>
-      <div className={ styles.content }>
-        <form className={ styles.form }>
-          <button type="button" className={ styles.closeBtn } onClick={ onClose }>
-            <img className={ styles.closeIcon } src={ closeIcon } alt="close"/>
+    <div className={styles.modal}>
+      <div className={styles.overlay} />
+      <div className={styles.content}>
+        <form className={styles.form}>
+          <button type="button" className={styles.closeBtn} onClick={onClose}>
+            <img className={styles.closeIcon} src={closeIcon} alt="close" />
           </button>
-          <div className={ styles.group }>
+          <div className={styles.group}>
             <h1 className="title">What’s your rating?</h1>
           </div>
-          <div className={ styles.group }>
-            <label className={ styles.label } htmlFor="form__rating">
+          <div className={styles.group}>
+            <label className={styles.label} htmlFor="form__rating">
               Rating
             </label>
             <div className="flex">
-              <Ratings value={ 0 }/>
+              <Ratings
+                value={ratings}
+                onChange={handleRatingsChange}
+                isActive={true}
+              />
             </div>
-            <div className={ styles.error }>Please select a rating</div>
+            <div className={styles.error}>Please select a rating</div>
           </div>
-          <div className={ styles.group }>
-            <label className={ styles.label } htmlFor="form__text">
+          <div className={styles.group}>
+            <label className={styles.label} htmlFor="form__text">
               Review
             </label>
             <textarea
               id="form__text"
-              className={ styles.textarea }
+              className={styles.textarea}
               placeholder="Start typing..."
               maxLength="50"
             />
